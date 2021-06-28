8ed7e63d8631f41b5c6f9affb2ec1792f1427194
kdh <troth1532@naver.com>
2021-06-29 00:07:09 +0900

    Spring Cloud Gateway Filter route

--- first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,19 +1,30 @@
 package com.example.firstservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 // zuul
 // http://localhost:8081/welcome
-// spring cloud gateway
+// spring cloud gateway 
 // http://localhost:8081/first-service/welcome
 
 @RestController
 @RequestMapping("/first-service")
+@Slf4j
 public class FirstServiceController {
 	@GetMapping("/welcome")
 	public String welcome() {
-		return "Welcome to the First Service";
+		return "Welcome to the First Service.";
+	}
+
+	@GetMapping("/message")
+	public String message(@RequestHeader("first-request") String header) {
+		log.info(header);
+
+		return "Welcome to the First Service.";
 	}
 }

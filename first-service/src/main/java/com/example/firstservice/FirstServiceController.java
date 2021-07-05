16253f4e1fc1e1d68c81861601b9ff96999a716f
kdh <troth1532@naver.com>
2021-07-05 22:08:22 +0900

    Spring Cloud Gateway + eureka LB TEST

--- first-service/src/main/java/com/example/firstservice/FirstServiceController.java
+++ first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,5 +1,8 @@
 package com.example.firstservice;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +19,14 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/first-service")
 @Slf4j
 public class FirstServiceController {
+	
+	Environment env;
+	
+	public FirstServiceController(Environment env) {
+		// TODO Auto-generated constructor stub
+		this.env = env;
+	}
+	
 	@GetMapping("/welcome")
 	public String welcome() {
 		return "Welcome to the First Service.";
@@ -29,7 +40,8 @@ public class FirstServiceController {
 	}
 	
 	@GetMapping("/check")
-	public String check() {
-		return "Hi, there, This is a message from First Servie.";
+	public String check(HttpServletRequest request) {
+		log.info("Server Port={}", request.getServerPort());
+		return String.format("Hi, there, This is a message from First Servie on PORT %s.",env.getProperty("local.server.port"));
 	}
 }
